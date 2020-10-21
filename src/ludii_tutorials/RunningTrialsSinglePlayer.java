71140172022c13537420cf90d9de604c0cedc32b
David Dunleavy <dunleavyd14@gmail.com>
2020-10-20 21:20:53 -0400

    Fixed trial entrypoints. There is an issue with the way search depth is passed to the AIs that will make using MCTS as the 'control' difficult.

--- src/ludii_tutorials/RunningTrialsSinglePlayer.java
+++ src/ludii_tutorials/RunningTrialsSinglePlayer.java
@@ -10,7 +10,6 @@ import java.util.List;
 import game.Game;
 import main.FileHandling;
 import search.mcts.nodes.BaseNode;
-import search.minimax.AlphaBetaSearch;
 import util.AI;
 import util.Context;
 import util.GameLoader;
@@ -18,15 +17,14 @@ import util.Trial;
 import util.model.Model;
 import utils.RandomAI;
 import search.mcts.MCTS;
-import search.minimax.AlphaBetaSearch;
 /**
  * Example class showing how we can run trials in Ludii
  *
  * @author Dennis Soemers
  */
-public class RunningTrialsTwoPlayer
+public class RunningTrialsSinglePlayer
 {
-
+	
 	/** The number of trials that we'd like to run */
 
 	/**
@@ -35,14 +33,14 @@ public class RunningTrialsTwoPlayer
 	 */
 	public static void main(final String[] args)
 	{
+		// Load our game -- we only need to do this once, and can use it for many trials
 		if (args.length < 4) {
 			System.out.println("Usage: java RunningTrials gameIdx numTrials maxSearchDepth maxCPUTime(in seconds)");
 			return;
 		}
-		// Load our game -- we only need to do this once, and can use it for many trials
 		int gameIdx = Integer.parseInt(args[0]);
 		String gameTitle = "";
-		try (BufferedReader br = new BufferedReader(new FileReader("LudiiTutorials/src/ludii_tutorials/TwoPlayer.txt"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader("LudiiTutorials/src/ludii_tutorials/SinglePlayer.txt"))) {
 			for (int i = 0; i < gameIdx; i++) {
 				br.readLine();
 			}
@@ -56,7 +54,7 @@ public class RunningTrialsTwoPlayer
 
 		System.out.println(gameTitle);
 		final Game game = GameLoader.loadGameFromName(gameTitle);
-
+		
 		// Prepare Context and Trial objects; these are also re-usable by resetting them,
 		// but we'd have to copy them if we wanted to preserve all of the different objects
 		// corresponding to different trials
@@ -66,37 +64,32 @@ public class RunningTrialsTwoPlayer
 
 		final int numTrials = Integer.parseInt(args[1]);
 		final int maxSearchDepth = Integer.parseInt(args[2]);
-		final double maxSeconds = Integer.parseInt(args[3]);
-
-		final int controlAIDepth = 10; //for now unused because only one depth can be passed, unlike cpu time
-		final double controlAITimeLimit = .01;
+		final double maxSeconds = Double.parseDouble(args[3]);
 
 		System.out.println(String.format("Max depth: %d, cpuTime: %f", maxSearchDepth, maxSeconds));
 		// Create AI objects that we'd like to use to play our Trials
 		// Here we just use Ludii's built in Random AI, because it's fast
 		// Ludii uses 1-based indexing for players, so we insert a null in the list first
+		MCTS p1 = MCTS.createTrackedUCT();
 		final List<AI> ais = new ArrayList<AI>();
 		ais.add(null);
-		MCTS p1 = MCTS.createTrackedUCT();
 		ais.add(p1);
-		ais.add(new RandomAI()); //using this because the forward model takes only one parameter for maxDepth. as far
-								// as I can tell, no overload lets me specify a specific depth per AI.
-
 
 		// Now we play through multiple trials
 		for (int i = 0; i < numTrials; ++i)
 		{
 			// This starts a new trial (resetting the Context and Trial objects if necessary)
+
 			game.start(context);
 			System.out.println("Starting a new trial!");
-
+			
 			// Random AI technically doesn't require initialisation, but it's good practice to do so
 			// for all AIs at the start of every new trial
 			for (int p = 1; p <= game.players().count(); ++p)
 			{
 				ais.get(p).initAI(game, p);
 			}
-
+			
 			// This "model" object lets us go through a trial step-by-step using a single API
 			// that works correctly for alternating-move as well as simultaneous-move games
 			final Model model = context.model();
@@ -104,7 +97,6 @@ public class RunningTrialsTwoPlayer
 			//these next two lines are a result of using the overload in the while loop. see comment in while loop
 			double[] timeLimits = new double[context.game().players().count() + 1];
 			Arrays.fill(timeLimits, maxSeconds);
-			timeLimits[2] = controlAITimeLimit;
 
 			// We keep looping for as long as the trial is not over
 			while (!trial.over())
@@ -118,11 +110,11 @@ public class RunningTrialsTwoPlayer
 				//this is an overload whose implementation I don't believe is open source
 				//I found this by looking at the decompiled class files.
 			}
-
+			
 			// When we reach this code, we know that the trial is over and we can see what ranks the
 			// different players achieved
 			final double[] ranking = trial.ranking();
-
+			
 			for (int p = 1; p <= game.players().count(); ++p)
 			{
 				// Here we print the rankings as achieved by every agent, where
